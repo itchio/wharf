9f93e31e1684843594f38db8776b245cffaca4a3
Amos Wenger <fasterthanlime@gmail.com>
2016-01-27 22:57:25 +0100

    Use 4KB blocks

--- pwr/constants.go
+++ pwr/constants.go
@@ -3,7 +3,7 @@ package pwr
 import "encoding/binary"
 
 var ENDIANNESS = binary.LittleEndian // gotta pick one
-var BLOCK_SIZE = 64 * 1024           // use 4KB blocks
+var BLOCK_SIZE = 4 * 1024
 
 const (
 	PWR_MAGIC = int32(iota + 0xFEF5F00)
