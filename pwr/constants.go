470fcc034e8e5ab29b220180606880d9be1e179a
Amos Wenger <fasterthanlime@gmail.com>
2016-01-29 15:07:51 +0100

    :memo:

--- pwr/constants.go
+++ pwr/constants.go
@@ -12,8 +12,6 @@ const (
 	pwrMagic = int32(iota + 0xFEF5F00)
 )
 
-// BlockSize is a compromise between wasted hashing work (because of padding)
-// and inefficient diffs
 var BlockSize = 64 * 1024 // 64k
 
 func mksync() *sync.SyncContext {
