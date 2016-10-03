e794728e8ce2c04b42d93592d5f9ba6d3cd4f22a
Amos Wenger <amoswenger@gmail.com>
2016-10-03 15:06:22 +0200

    Introduce woundsprinter, change Wound format, parallel validator

--- pwr/validator.go
+++ pwr/validator.go
@@ -3,42 +3,54 @@ package pwr
 import (
 	"io"
 	"os"
+	"runtime"
 
-	humanize "github.com/dustin/go-humanize"
 	"github.com/go-errors/errors"
 	"github.com/itchio/wharf/pools"
 	"github.com/itchio/wharf/pools/nullpool"
+	"github.com/itchio/wharf/tlc"
+	"github.com/itchio/wharf/wsync"
 )
 
 type ValidatorContext struct {
 	WoundsPath string
+	NumWorkers int
 
 	Consumer *StateConsumer
+
+	// internal
+	Container      *tlc.Container
+	TargetPool     wsync.Pool
+	ValidatingPool wsync.WritablePool
+	Wounds         chan *Wound
 }
 
-func (vc *ValidatorContext) Validate(target string, signature *SignatureInfo) error {
+func (vctx *ValidatorContext) Validate(target string, signature *SignatureInfo) error {
 	var woundsWriter *WoundsWriter
-	wounds := make(chan *Wound)
+	vctx.Wounds = make(chan *Wound)
 	errs := make(chan error)
 	done := make(chan bool)
 
-	if vc.WoundsPath == "" {
+	if vctx.WoundsPath == "" {
+		woundsPrinter := &WoundsPrinter{
+			Wounds: vctx.Wounds,
+		}
+
 		go func() {
-			for wound := range wounds {
-				file := signature.Container.Files[wound.FileIndex]
-				woundSize := humanize.IBytes(uint64(wound.BlockSpan * int64(BlockSize)))
-				offset := humanize.IBytes(uint64(wound.BlockIndex * int64(BlockSize)))
-				vc.Consumer.Infof("~%s wound %s into %s", woundSize, offset, file.Path)
+			err := woundsPrinter.Do(signature, vctx.Consumer)
+			if err != nil {
+				errs <- err
+				return
 			}
 			done <- true
 		}()
 	} else {
 		woundsWriter = &WoundsWriter{
-			Wounds: wounds,
+			Wounds: vctx.Wounds,
 		}
 
 		go func() {
-			err := woundsWriter.Go(signature, vc.WoundsPath)
+			err := woundsWriter.Do(signature, vctx.WoundsPath)
 			if err != nil {
 				errs <- err
 				return
@@ -47,77 +59,102 @@ func (vc *ValidatorContext) Validate(target string, signature *SignatureInfo) er
 		}()
 	}
 
-	targetPool, err := pools.New(signature.Container, target)
+	var err error
+	vctx.TargetPool, err = pools.New(signature.Container, target)
 	if err != nil {
 		return err
 	}
 
-	validatingPool := &ValidatingPool{
+	vctx.Container = signature.Container
+
+	vctx.ValidatingPool = &ValidatingPool{
 		Pool:      nullpool.New(signature.Container),
 		Container: signature.Container,
 		Signature: signature,
 
-		Wounds: wounds,
+		Wounds: vctx.Wounds,
 	}
 
-	for i, f := range signature.Container.Files {
-		fileIndex := int64(i)
-
-		reader, err := targetPool.GetReader(fileIndex)
-		if err != nil {
-			if os.IsNotExist(err) {
-				// whole file is missing!
-				wounds <- &Wound{
-					FileIndex:  fileIndex,
-					BlockIndex: 0,
-					BlockSpan:  numBlocks(f.Size),
-				}
-				continue
-			} else {
-				return err
-			}
-		}
-
-		writer, err := validatingPool.GetWriter(fileIndex)
-		if err != nil {
-			return errors.Wrap(err, 1)
-		}
-
-		writtenBytes, err := io.Copy(writer, reader)
-		if err != nil {
-			return err
-		}
-
-		if writtenBytes != f.Size {
-			totalBlocks := numBlocks(f.Size)
-			startBlock := writtenBytes / int64(BlockSize)
-			wounds <- &Wound{
-				FileIndex:  fileIndex,
-				BlockIndex: startBlock,
-				BlockSpan:  totalBlocks - startBlock,
-			}
-			vc.Consumer.Infof("short file: expected %d, got %d", writtenBytes, f.Size)
-		}
-
-		err = writer.Close()
-		if err != nil {
-			return errors.Wrap(err, 1)
-		}
+	numWorkers := vctx.NumWorkers
+	if numWorkers == 0 {
+		numWorkers = runtime.NumCPU() + 1
 	}
 
-	err = targetPool.Close()
+	fileIndices := make(chan int64)
+
+	for i := 0; i < numWorkers; i++ {
+		go vctx.validate(fileIndices, done, errs)
+	}
+
+	for fileIndex := range signature.Container.Files {
+		fileIndices <- int64(fileIndex)
+	}
+
+	err = vctx.TargetPool.Close()
 	if err != nil {
 		return errors.Wrap(err, 1)
 	}
 
-	close(wounds)
+	close(vctx.Wounds)
 
-	select {
-	case err := <-errs:
-		return err
-	case <-done:
-		// good!
+	for i := 0; i < numWorkers+1; i++ {
+		select {
+		case err := <-errs:
+			return err
+		case <-done:
+			// good!
+		}
 	}
 
 	return nil
+}
+
+func (vctx *ValidatorContext) validate(fileIndices chan int64, done chan bool, errs chan error) {
+	for fileIndex := range fileIndices {
+		file := vctx.Container.Files[fileIndex]
+
+		reader, err := vctx.TargetPool.GetReader(fileIndex)
+		if err != nil {
+			if os.IsNotExist(err) {
+				// that's one big wound
+				vctx.Wounds <- &Wound{
+					FileIndex: fileIndex,
+					Start:     0,
+					End:       file.Size,
+				}
+				continue
+			} else {
+				errs <- err
+				return
+			}
+		}
+
+		var writer io.WriteCloser
+		writer, err = vctx.ValidatingPool.GetWriter(fileIndex)
+		if err != nil {
+			errs <- errors.Wrap(err, 1)
+			return
+		}
+
+		writtenBytes, err := io.Copy(writer, reader)
+		if err != nil {
+			errs <- errors.Wrap(err, 1)
+			return
+		}
+
+		if writtenBytes != file.Size {
+			vctx.Wounds <- &Wound{
+				FileIndex: fileIndex,
+				Start:     writtenBytes,
+				End:       file.Size,
+			}
+			vctx.Consumer.Infof("short file: expected %d, got %d", writtenBytes, file.Size)
+		}
+
+		err = writer.Close()
+		if err != nil {
+			errs <- errors.Wrap(err, 1)
+			return
+		}
+	}
 }
