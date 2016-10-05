0912091870c9246620379b3104043b3892cf69a7
Amos Wenger <amos@itch.io>
2016-10-05 17:42:47 +0200

    Exclude cbrotli from coverage reporting because mingw does *not* like multiple definitions by default

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -2,17 +2,21 @@
 
 go version
 
+rm -rf src pkg bin
+
 export CURRENT_BUILD_PATH=$(pwd)
 export GOPATH=$CURRENT_BUILD_PATH
 export PKG=github.com/itchio/wharf
-
 export PATH=$PATH:$GOPATH/bin
 
 mkdir -p src/$PKG
 rsync -a --exclude 'src' . src/$PKG
-go get -v -t $PKG/...
 
-go list -f '{{if gt (len .TestGoFiles) 0}}"go test -covermode count -coverprofile {{.Name}}.coverprofile -coverpkg $PKG/... {{.ImportPath}}"{{end}}' $PKG/... | xargs -I {} bash -c {}
+export PKGS="$(go list -f '{{if not (eq .Name "cbrotli")}}{{.ImportPath}}{{end}}' $PKG/... | paste -s -d ',' -)"
+
+go get -v -d -t $PKG/...
+
+go list -f '{{if gt (len .TestGoFiles) 0}}"go test -covermode count -coverprofile {{.Name}}.coverprofile -coverpkg $PKGS {{.ImportPath}}"{{end}}' $PKG/... | xargs -I {} bash -c {}
 
 go get -v github.com/wadey/gocovmerge
 
