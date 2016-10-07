55afcd5c8b16e4e32e1fa7fb5038741fe7567fe5
Amos Wenger <amoswenger@gmail.com>
2016-10-07 15:54:48 +0200

    dripwriter test

---
+++ pwr/dripwriter_test.go
@@ -0,0 +1,64 @@
+package pwr
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/alecthomas/assert"
+	"github.com/itchio/wharf/counter"
+)
+
+func Test_DripWriter(t *testing.T) {
+	dropSize := 16
+
+	numShort := 0
+	shortSize := 0
+	validate := func(buf []byte) error {
+		switch {
+		case len(buf) == dropSize:
+			if numShort > 0 {
+				return fmt.Errorf("got full after short")
+			}
+			return nil
+		case len(buf) < dropSize:
+			if numShort > 0 {
+				return fmt.Errorf("got second short (%d)", len(buf))
+			}
+			numShort++
+			shortSize = len(buf)
+		default:
+			return fmt.Errorf("drop too large (%d > %d)", len(buf), dropSize)
+		}
+
+		return nil
+	}
+
+	buf := make([]byte, dropSize)
+	countingWriter := counter.NewWriter(nil)
+
+	dw := &DripWriter{
+		Buffer:   buf,
+		Validate: validate,
+		Writer:   countingWriter,
+	}
+
+	rbuf := make([]byte, 128)
+
+	write := func(l int) {
+		written, wErr := dw.Write(rbuf[0:l])
+		assert.Equal(t, l, written)
+		assert.Nil(t, wErr)
+	}
+
+	write(12)
+	write(4)
+	write(10)
+	write(6)
+	write(16)
+	write(64)
+	write(5)
+
+	assert.Nil(t, dw.Close())
+	assert.Equal(t, 5, shortSize)
+	assert.Equal(t, int64(12+4+10+6+16+64+5), countingWriter.Count())
+}
