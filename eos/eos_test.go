ce184c44803be52b338a65576e60cd1eb841f070
Amos Wenger <amoswenger@gmail.com>
2016-10-09 12:45:25 +0200

    Let the httpfile package test itself, concentrate on eos

--- eos/eos_test.go
+++ eos/eos_test.go
@@ -1,11 +1,13 @@
 package eos
 
 import (
-	"bytes"
+	"fmt"
 	"io/ioutil"
-	"math/rand"
 	"net/http"
+	"net/http/httptest"
 	"net/url"
+	"os"
+	"path/filepath"
 	"testing"
 	"time"
 
@@ -14,256 +16,119 @@ import (
 	"github.com/itchio/httpfile"
 )
 
-func Test_OpenLocalFile(t *testing.T) {
+func Test_OpenEmptyFile(t *testing.T) {
 	f, err := Open("/dev/null")
-	assert.Nil(t, err)
+	assert.NoError(t, err)
 
 	s, err := f.Stat()
-	assert.Nil(t, err)
+	assert.NoError(t, err)
 
-	assert.Equal(t, int64(0), s.Size())
-	assert.Nil(t, f.Close())
+	assert.EqualValues(t, 0, s.Size())
+	assert.False(t, s.IsDir())
+	assert.EqualValues(t, "/dev/null", s.Name())
+	assert.Nil(t, s.Sys())
+	assert.EqualValues(t, 0644, s.Mode())
+	assert.EqualValues(t, time.Time{}, s.ModTime())
+
+	buf := make([]byte, 1)
+
+	_, err = f.Read(buf)
+	assert.Error(t, err)
+
+	_, err = f.ReadAt(buf, 0)
+	assert.Error(t, err)
+
+	assert.NoError(t, f.Close())
 }
 
-type itchfs struct {
+func Test_OpenLocalFile(t *testing.T) {
+	mainDir, err := ioutil.TempDir("", "eos-local")
+	assert.NoError(t, err)
+	defer os.RemoveAll(mainDir)
+
+	assert.NoError(t, os.MkdirAll(mainDir, 0755))
+
+	fileName := filepath.Join(mainDir, "some-file")
+	assert.NoError(t, ioutil.WriteFile(fileName, []byte{4, 2, 6, 9}, 0644))
+
+	f, err := Open(fileName)
+	assert.NoError(t, err)
+
+	s, err := f.Stat()
+	assert.NoError(t, err)
+
+	assert.EqualValues(t, 4, s.Size())
+	assert.NoError(t, f.Close())
+}
+
+type testfs struct {
 	url string
 }
 
-func (ifs *itchfs) Scheme() string {
-	return "itchfs"
+func (tfs *testfs) Scheme() string {
+	return "testfs"
 }
 
-func (ifs *itchfs) MakeResource(u *url.URL) (httpfile.GetURLFunc, httpfile.NeedsRenewalFunc, error) {
-	return ifs.GetURL, ifs.NeedsRenewal, nil
+func (tfs *testfs) MakeResource(u *url.URL) (httpfile.GetURLFunc, httpfile.NeedsRenewalFunc, error) {
+	return tfs.GetURL, tfs.NeedsRenewal, nil
 }
 
-func (ifs *itchfs) GetURL() (string, error) {
-	return ifs.url, nil
+func (tfs *testfs) GetURL() (string, error) {
+	return tfs.url, nil
 }
 
-func (ifs *itchfs) NeedsRenewal(req *http.Request) bool {
+func (tfs *testfs) NeedsRenewal(req *http.Request) bool {
 	return false
 }
 
 func Test_OpenRemoteDownloadBuild(t *testing.T) {
 	fakeData := []byte("aaaabbbb")
 
-	storageServer := fakeStorage(t, fakeData, time.Duration(0))
-	defer storageServer.CloseClientConnections()
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("content-length", fmt.Sprintf("%d", len(fakeData)))
+		w.WriteHeader(200)
+		w.Write(fakeData)
+	}))
+	defer server.CloseClientConnections()
 
-	ifs := &itchfs{storageServer.URL}
-	assert.Nil(t, RegisterHandler(ifs))
-	assert.NotNil(t, RegisterHandler(ifs))
+	tfs := &testfs{server.URL}
+	assert.NoError(t, RegisterHandler(tfs))
+	assert.Error(t, RegisterHandler(tfs))
+	defer DeregisterHandler(tfs)
 
-	defer DeregisterHandler(ifs)
+	f, err := Open("nofs:///not-quite")
+	assert.Error(t, err)
 
-	f, err := Open("itchfs:///upload/187770/download/builds/6996?api_key=foo")
-	assert.Nil(t, err)
+	f, err = Open("testfs:///now/we/are/talking")
+	assert.NoError(t, err)
 
-	s, err := f.Stat()
-	assert.Nil(t, err)
-	assert.Equal(t, int64(len(fakeData)), s.Size())
+	stats, err := f.Stat()
+	assert.NoError(t, err)
 
-	readFakeData, err := ioutil.ReadAll(f)
-	assert.Nil(t, err)
-	assert.Equal(t, len(fakeData), len(readFakeData))
-	assert.Equal(t, fakeData, readFakeData)
-
-	readBytes, err := f.ReadAt(readFakeData, 0)
-	assert.Nil(t, err)
-	assert.Equal(t, len(fakeData), readBytes)
-	assert.Equal(t, fakeData, readFakeData)
-
-	err = f.Close()
-	if err != nil {
-		t.Fatal(err)
-		t.FailNow()
-	}
+	assert.EqualValues(t, len(fakeData), stats.Size())
+	assert.NoError(t, f.Close())
 }
 
 func Test_HttpFile(t *testing.T) {
 	fakeData := []byte("aaaabbbb")
 
-	storageServer := fakeStorage(t, fakeData, time.Duration(0))
-	defer storageServer.CloseClientConnections()
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("content-length", fmt.Sprintf("%d", len(fakeData)))
+		w.WriteHeader(200)
+		w.Write(fakeData)
+	}))
+	defer server.CloseClientConnections()
 
-	f, err := Open(storageServer.URL)
-	assert.Nil(t, err)
-
-	s, err := f.Stat()
-	assert.Nil(t, err)
-	assert.Equal(t, int64(len(fakeData)), s.Size())
-
-	buf := make([]byte, 4)
-	readBytes, err := f.ReadAt(buf, 4)
-	assert.Nil(t, err)
-	assert.Equal(t, len(buf), readBytes)
-	assert.Equal(t, []byte("bbbb"), buf)
-
-	err = f.Close()
-	if err != nil {
-		t.Fatal(err)
-		t.FailNow()
-	}
-}
-
-var _bigFakeData []byte
-
-func getBigFakeData() []byte {
-	if _bigFakeData == nil {
-		for i := 1; i < 5; i++ {
-			_bigFakeData = append(_bigFakeData, bytes.Repeat([]byte{byte(i * 10)}, 1*1024*1024)...)
-		}
-	}
-
-	return _bigFakeData
-}
-
-func Test_HttpFileSequentialReads(t *testing.T) {
-	fakeData := getBigFakeData()
-
-	storageServer := fakeStorage(t, fakeData, time.Duration(0))
-	defer storageServer.CloseClientConnections()
-
-	f, err := Open(storageServer.URL)
-	assert.Nil(t, err)
-
-	hf, ok := f.(*httpfile.HTTPFile)
-	assert.True(t, ok)
-
-	hf.ReaderStaleThreshold = time.Millisecond * time.Duration(100)
-
-	readBuf := make([]byte, 256)
-	offset := int64(0)
-	readIndex := 0
-
-	sequentialReadStop := int64(len(readBuf) * 10)
-
-	for offset < sequentialReadStop {
-		readIndex++
-
-		if readIndex%4 == 0 {
-			offset += int64(len(readBuf))
-			continue
-		}
-
-		readBytes, err := f.ReadAt(readBuf, offset)
-		assert.Nil(t, err)
-		assert.Equal(t, len(readBuf), readBytes)
-
-		offset += int64(readBytes)
-	}
-
-	assert.Equal(t, 1, hf.NumReaders())
-
-	// forcing to provision a new reader
-	readBytes, err := f.ReadAt(readBuf, 0)
-	assert.Nil(t, err)
-	assert.Equal(t, len(readBuf), readBytes)
-
-	assert.Equal(t, 2, hf.NumReaders())
-
-	// re-using the first one
-	readBytes, err = f.ReadAt(readBuf, sequentialReadStop+int64(len(readBuf)))
-	assert.Nil(t, err)
-	assert.Equal(t, len(readBuf), readBytes)
-
-	assert.Equal(t, 2, hf.NumReaders())
-
-	// forcing a third one
-	readBytes, err = f.ReadAt(readBuf, int64(len(fakeData))-int64(len(readBuf)))
-	assert.Nil(t, err)
-	assert.Equal(t, len(readBuf), readBytes)
-
-	assert.Equal(t, 3, hf.NumReaders())
-
-	// re-using second one
-	readBytes, err = f.ReadAt(readBuf, int64(len(readBuf)))
-	assert.Nil(t, err)
-	assert.Equal(t, len(readBuf), readBytes)
-
-	assert.Equal(t, 3, hf.NumReaders())
-
-	// and again, skipping a few
-	readBytes, err = f.ReadAt(readBuf, int64(len(readBuf)*3))
-	assert.Nil(t, err)
-	assert.Equal(t, len(readBuf), readBytes)
-
-	assert.Equal(t, 3, hf.NumReaders())
-
-	// wait for readers to become stale
-	time.Sleep(time.Millisecond * time.Duration(200))
-
-	// now just read something random, should be back to 1 reader
-	readBytes, err = f.ReadAt(readBuf, 0)
-	assert.Nil(t, err)
-	assert.Equal(t, len(readBuf), readBytes)
-	assert.Equal(t, 1, hf.NumReaders())
-
-	err = f.Close()
-	assert.Nil(t, err)
-}
-
-func Test_HttpFileConcurrentReadAt(t *testing.T) {
-	fakeData := []byte("abcdefghijklmnopqrstuvwxyz")
-
-	storageServer := fakeStorage(t, fakeData, time.Duration(10)*time.Millisecond)
-	defer storageServer.CloseClientConnections()
-
-	f, err := Open(storageServer.URL)
-	assert.Nil(t, err)
-
-	hf, ok := f.(*httpfile.HTTPFile)
-	assert.True(t, ok)
+	f, err := Open(server.URL)
+	assert.NoError(t, err)
 
 	s, err := f.Stat()
-	assert.Nil(t, err)
-	assert.Equal(t, int64(len(fakeData)), s.Size())
+	assert.NoError(t, err)
+	assert.EqualValues(t, len(fakeData), s.Size())
 
-	done := make(chan bool)
-	errs := make(chan error)
+	readData, err := ioutil.ReadAll(f)
+	assert.NoError(t, err)
+	assert.EqualValues(t, fakeData, readData)
 
-	rand.Seed(0xDEADBEEF)
-	for i := range rand.Perm(len(fakeData)) {
-		go func(i int) {
-			buf := make([]byte, 1)
-			readBytes, err := f.ReadAt(buf, int64(i))
-			if err != nil {
-				errs <- err
-				return
-			}
-
-			assert.Equal(t, readBytes, 1)
-			assert.Equal(t, string(buf), string(fakeData[i:i+1]))
-
-			done <- true
-		}(i)
-	}
-
-	maxReaders := 0
-
-	for i := 0; i < len(fakeData); i++ {
-		numReaders := hf.NumReaders()
-		if numReaders > maxReaders {
-			maxReaders = numReaders
-		}
-
-		select {
-		case err := <-errs:
-			t.Fatal(err)
-			t.FailNow()
-		case <-done:
-			// good!
-		}
-	}
-
-	t.Logf("maximum number of readers: %d (total reads: %d)", maxReaders, len(fakeData))
-
-	err = f.Close()
-	if err != nil {
-		t.Fatal(err)
-		t.FailNow()
-	}
-
-	assert.Equal(t, 0, hf.NumReaders())
+	assert.NoError(t, f.Close())
 }
