1d52b5c1c088cf38246e32573e0aa35f8def4695
Amos Wenger <amos@itch.io>
2018-04-16 12:32:54 +0200

    Simplify ci script

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -9,11 +9,9 @@ export PKG=github.com/itchio/wharf
 export PATH=$PATH:$GOPATH/bin
 
 mkdir -p $GOPATH/src/$PKG
-rsync -a --exclude 'src' . $GOPATH/src/$PKG
+rsync -a --exclude 'gopath' . $GOPATH/src/$PKG
 
 go get -v -d -t $PKG/...
-
-# test with race detector
 go test -v -cover -coverprofile=coverage.txt -race $PKG/...
 
 curl -s https://codecov.io/bash | bash
