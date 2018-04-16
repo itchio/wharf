7d4e610fdbd5ca122236da00e36cbd62f6e250e4
Amos Wenger <amos@itch.io>
2018-04-16 11:10:59 +0200

    Simplify coverage script

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -2,29 +2,18 @@
 
 go version
 
-rm -rf src pkg bin
+export GOPATH=$PWD/gopath
+rm -rf $GOPATH
 
-export CURRENT_BUILD_PATH=$(pwd)
-export GOPATH=$CURRENT_BUILD_PATH
 export PKG=github.com/itchio/wharf
 export PATH=$PATH:$GOPATH/bin
 
-mkdir -p src/$PKG
-rsync -a --exclude 'src' . src/$PKG
-
-export PKGS="$(go list -f '{{if not (eq .Name "cbrotli")}}{{.ImportPath}}{{end}}' $PKG/... | paste -s -d ',' -)"
+mkdir -p $GOPATH/src/$PKG
+rsync -a --exclude 'src' . $GOPATH/src/$PKG
 
 go get -v -d -t $PKG/...
 
 # test with race detector
-go test -v -race $PKG/...
-
-# ... then do coverage
-go list -f '{{if gt (len .TestGoFiles) 0}}"go test -v -coverprofile {{.Name}}.coverprofile -coverpkg $PKGS {{.ImportPath}}"{{end}}' $PKG/... | xargs -I {} bash -c {}
-
-go get -v github.com/wadey/gocovmerge
-
-gocovmerge `ls *.coverprofile` > coverage.txt
-go tool cover -func=coverage.txt
+go test -v -cover -coverprofile=coverage.txt -race $PKG/...
 
 curl -s https://codecov.io/bash | bash
