7d5895e8f363a74b668753d4530bfb283d3b427e
Amos Wenger <amoswenger@gmail.com>
2020-03-03 21:48:11 +0100

    Make tests more low-memory-friendly

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -4,6 +4,12 @@ go version
 
 export PATH="$PATH:$(go env GOPATH)/bin"
 
-go test -v -race -cover -coverprofile=coverage.txt ./...
+PACKAGES=$(go list -f "{{ .Dir }}" ./... | sed 's/[\]/\//g' | sed 's/.*wharf[/]//')
+
+for PKG in ${PACKAGES}; do
+    go test -v -race ./$PKG
+done
+
+go test -cover -coverprofile=coverage.txt ./...
 
 curl -s https://codecov.io/bash | bash
